77acccc86da2bd233172d4a2286c40189994e75e
Jetstream <bigalex934@gmail.com>
2021-10-05 20:52:12 +0300

    Append QApplication

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+#include <QApplication>
 
-int main()
+int main(int argc, char * argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    QApplication app(argc, argv);
+
+    return app.exec();
 }
