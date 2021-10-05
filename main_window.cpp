5a34ccc3de776b04543809c3518e37a923c78545
Jetstream <bigalex934@gmail.com>
2021-10-05 21:01:28 +0300

    Append view widget and his graphics scene

--- main_window.cpp
+++ main_window.cpp
@@ -2,5 +2,16 @@
 
 MainWindow::MainWindow(QMainWindow * parent) : QMainWindow(parent)
 {
+    initUI();
+}
 
+void MainWindow::initUI()
+{
+    try {
+        _scene = new QGraphicsScene(this);
+        _view = new QGraphicsView(_scene);
+    } catch (std::bad_alloc& ) {
+        std::terminate();
+    }
+    setCentralWidget(_view);
 }
--- main_window.h
+++ main_window.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QGraphicsView>
+#include <QGraphicsScene>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit MainWindow(QMainWindow * parent = nullptr);
+private:
+    void initUI();
+
+    QGraphicsView * _view;
+    QGraphicsScene * _scene;
 };
