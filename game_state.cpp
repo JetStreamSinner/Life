9b9e421c11ae8e0d47e99bea1f627793d2ff0468
Jetstream <bigalex934@gmail.com>
2021-10-07 23:44:19 +0300

    Implement base game logic in GameState

--- game_state.cpp
+++ game_state.cpp
@@ -1,13 +1,17 @@
 #include <stdexcept>
+#include <algorithm>
 #include "game_state.h"
 
 
 GameState::GameState(int rows, int columns) : _rowCount(rows), _columnCount(columns)
 {
-
+    _cellsStates.resize(_rowCount);
+    std::for_each(_cellsStates.begin(), _cellsStates.end(), [&](auto &row) {
+        row.resize(_columnCount);
+    });
 }
 
-bool GameState::validateState(const PlayingField& field)
+bool GameState::validateField(const PlayingField& field) const
 {
     bool fieldHasRows = !field.empty();
     if (fieldHasRows) {
@@ -21,9 +25,75 @@ bool GameState::validateState(const PlayingField& field)
     return true;
 }
 
+bool GameState::cellExist(int rowIndex, int columnIndex) const
+{
+    bool rowIndexValid = rowIndex >= 0 && rowIndex < _rowCount;
+    bool columnIndexValid = columnIndex >= 0 && columnIndex < _columnCount;
+    return rowIndexValid && columnIndexValid;
+}
+
+int GameState::aliveNeighborsCount(int targetRowIndex, int targetColumnIndex) const
+{
+    const auto baseRowIndex = targetRowIndex - 1;
+    const auto baseColumnIndex = targetColumnIndex - 1;
+
+    int rowIndex = baseRowIndex;
+    int columnIndex = baseColumnIndex;
+
+    auto leftBound = 0;
+    auto rightBound = 2;
+    auto bottomBound = 2;
+
+    auto neighborsCounter = 0;
+
+    // left to right
+    const auto topBound = 0;
+    for (auto index = leftBound; index <= rightBound; ++index)
+        if (cellExist(topBound, index))
+            neighborsCounter += _cellsStates.at(topBound).at(index).state() == CellState::Alive;
+
+    // top to bottom
+    for (auto index = topBound + 1; index <= bottomBound; ++index)
+        if (cellExist(index, rightBound))
+            neighborsCounter += _cellsStates.at(index).at(rightBound).state() == CellState::Alive;
+
+    // right to left
+    for (auto index = rightBound - 1; index >= leftBound; --index)
+        if (cellExist(bottomBound, index))
+            neighborsCounter += _cellsStates.at(bottomBound).at(index).state() == CellState::Alive;
+
+    // bottom to top
+    for (auto index = bottomBound + 1; index >= leftBound; --index)
+        if (cellExist(index, leftBound))
+            neighborsCounter += _cellsStates.at(index).at(leftBound).state() == CellState::Alive;
+
+    return neighborsCounter;
+}
+
+void GameState::enableCell(int rowIndex, int columnIndex)
+{
+    if (!cellExist(rowIndex, columnIndex))
+        throw std::logic_error("Wrong cell index");
+    _cellsStates.at(rowIndex).at(columnIndex).enable();
+}
+
+void GameState::disableCell(int rowIndex, int columnIndex)
+{
+    if (!cellExist(rowIndex, columnIndex))
+        throw std::logic_error("Wrong cell index");
+    _cellsStates.at(rowIndex).at(columnIndex).disable();
+}
+
+CellState GameState::cellState(int rowIndex, int columnIndex) const
+{
+    if (!cellExist(rowIndex, columnIndex))
+        throw std::logic_error("Wrong cell index");
+    return _cellsStates.at(rowIndex).at(columnIndex).state();
+}
+
 void GameState::setGameState(const PlayingField& updatedField)
 {
-    bool stateValid = validateState(updatedField);
+    bool stateValid = validateField(updatedField);
     if (!stateValid)
         throw std::logic_error("Wrong field size.");
 
@@ -41,5 +111,17 @@ PlayingField GameState::currentState() const
 
 void GameState::nextIteration()
 {
+    static const auto aliveCondition = 3;
+    PlayingField nextState(_cellsStates);
 
+    for (auto rowIndex = 0; rowIndex < _rowCount; ++rowIndex) {
+        for (auto columnIndex = 0; columnIndex < _columnCount; ++columnIndex) {
+            auto neighborsCount = aliveNeighborsCount(rowIndex, columnIndex);
+            if (neighborsCount == aliveCondition)
+                nextState.at(rowIndex).at(columnIndex).enable();
+            else
+                nextState.at(rowIndex).at(columnIndex).disable();
+        }
+    }
+    _cellsStates = nextState;
 }
--- game_state.h
+++ game_state.h
@@ -11,12 +11,19 @@ public:
     explicit GameState(int rows, int column);
     void nextIteration();
 
+    void enableCell(int rowIndex, int columnIndex);
+    void disableCell(int rowIndex, int columnIndex);
+    CellState cellState(int rowIndex, int columnIndex) const;
+
     void setGameState(const PlayingField &updatedField);
     PlayingField  currentState() const;
 
 private:
 
-    static bool validateState(const PlayingField &field) ;
+    bool validateField(const PlayingField &field) const;
+    bool cellExist(int rowIndex, int columnIndex) const;
+
+    int aliveNeighborsCount(int rowIndex, int columnIndex) const;
 
     int _rowCount;
     int _columnCount;
